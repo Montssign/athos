580583c294f3ba9ff3d859e041081dfb82e3e980
Felipe D S Lima <felipe@monts.com.br>
2020-05-07 20:09:41 -0300

    Mail configuration

---
+++ server/src/configs/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 587,
+	secure: false,
+	auth: {
+		user: 'd02e1473a98db8',
+		pass: '62a0c816bcf088',
+	},
+	default: {
+		from: 'Equipe Athos <contato@felipedslima.com.br>',
+	},
+}
