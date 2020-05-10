4d4b8bb4080226296daf0d013539a72a19b0b7e2
Felipe D S Lima <felipe@monts.com.br>
2020-05-10 17:17:52 -0300

    Adding queue

--- dev.js
+++ dev.js
@@ -1,6 +1,7 @@
 const { spawn } = require("child_process")
 
 const server = spawn("yarn", ["--cwd", "server", "dev"])
+const serverQueue = spawn("yarn", ["--cwd", "server", "queue"])
 const web = spawn("yarn", ["--cwd", "web", "start"])
 
 server.stdout.on("data", (data) => {
@@ -15,6 +16,17 @@ server.on("close", (data) => {
   console.log(`Server close: ${data}`)
 })
 
+serverQueue.stdout.on("data", (data) => {
+  console.log(`Server Queue: ${data}`)
+})
+
+serverQueue.stderr.on("data", (data) => {
+  console.log(`Server Queue error: ${data}`)
+})
+
+serverQueue.on("close", (data) => {
+  console.log(`Server Queue close: ${data}`)
+})
 
 web.stdout.on("data", (data) => {
   console.log(`Web: ${data}`)
